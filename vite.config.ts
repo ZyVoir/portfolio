acc4528043d96f26075a92bcd67c7fb45381de14
ZyVoir <148558860+ZyVoir@users.noreply.github.com>
2024-05-17 18:46:06 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { default as tailwindcss } from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +9,5 @@ export default defineConfig({
 	build: {
 		outDir: "dist",
 	},
+	base: "/Portofolio",
 });
