93e562aa7905f0c73ccf3fde236ad50363ccbbc3
ZyVoir <148558860+ZyVoir@users.noreply.github.com>
2024-05-25 02:51:50 +0700

    error changes to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import {
-	BrowserRouter,
-	RouterProvider,
-	createBrowserRouter,
-} from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import Education from "./pages/Education.tsx";
-import Experience from "./pages/Experience.tsx";
-import Projects from "./pages/Projects.tsx";
-import SendMsg from "./pages/SendMsg.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
