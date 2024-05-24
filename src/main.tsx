3b22a48787fce1c73db7122d3c8979ddb9938936
ZyVoir <148558860+ZyVoir@users.noreply.github.com>
2024-05-25 02:50:13 +0700

    set up browser router

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import {
+	BrowserRouter,
+	RouterProvider,
+	createBrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Education from "./pages/Education.tsx";
+import Experience from "./pages/Experience.tsx";
+import Projects from "./pages/Projects.tsx";
+import SendMsg from "./pages/SendMsg.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<BrowserRouter>
-		<React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
 			<App />
-		</React.StrictMode>
-	</BrowserRouter>
+		</BrowserRouter>
+	</React.StrictMode>
 );
