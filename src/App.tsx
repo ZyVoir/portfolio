e50a7df3f377b20d372cb618a8e68104847cb106
ZyVoir <148558860+ZyVoir@users.noreply.github.com>
2024-05-25 03:12:38 +0700

    fix hash router

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import Education from "./pages/Education";
 import Experience from "./pages/Experience";
 import Projects from "./pages/Projects";
 import SendMsg from "./pages/SendMsg";
-import { HashRouter as Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
-		<RootLayout>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/Education" element={<Education />} />
-				<Route path="/Experience" element={<Experience />} />
-				<Route path="/Projects" element={<Projects />} />
-				<Route path="/SendMsg" element={<SendMsg />} />
-			</Routes>
-		</RootLayout>
+		<Router>
+			<RootLayout>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/Education" element={<Education />} />
+					<Route path="/Experience" element={<Experience />} />
+					<Route path="/Projects" element={<Projects />} />
+					<Route path="/SendMsg" element={<SendMsg />} />
+				</Routes>
+			</RootLayout>
+		</Router>
 	);
 }
 
